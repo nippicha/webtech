7ca934c2809db28772adf8c465f13b29c92189fe
nippicha <nippicha.c@ku.th>
2019-02-23 05:23:18 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,71 @@
-<!DOCTYPE html>
+<?php
+    if(isset($_POST['done'])){
+        $item = $_POST['item'];
+        $price = $_POST['price'];
+
+        $file = fopen("01.csv","a");
+        $add_row = "/n".$item.",".$price;
+        fwrite($file,$add_row);
+        fclose($file);
+    }
+?>
 <html>
 <head>
-  <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shopping</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
-  <?php
-  <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.'</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+
+<body style="background-color: #efeeee;">
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="">Menu Bar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="01.csv" download="">Download</a>
+            </li>
+        </ul>
+    </nav>
+    <form method="post">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6 mx-auto">
+                        <div class="card rounded-0">
+                            <div class="card-header">
+                                <h3 class="title">Shopping Record</h3>
+                            </div>
+                            <div class="card-body">
+                                <form class="form" role="form" autocomplete="off" id="formLogin" method="post" enctype="multipart/form-data">
+
+                                    <div class="form-group">
+                                        <label for="uname1">Item</label>
+                                        <input type="text" class="form-control form-control-lg rounded-0" name="item" id="item" required="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="uname1">Price</label>
+                                        <input type="text" class="form-control form-control-lg rounded-0" name="price" id="price" required="">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                    </div>
+                                    <button type="submit" id="done" name="done" value="Submit" class="btn btn-outline-primary btn-lg float-right">Done</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </form>
-?>
 </body>
 </html>
