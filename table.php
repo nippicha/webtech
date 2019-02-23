b6394e2a8475ff2ad6748dca7118b7b25b05923e
nippicha <nippicha.c@ku.th>
2019-02-23 23:53:52 +0700

    Add files via upload

--- table.php
+++ table.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-sm bg-light navbar-light">
@@ -20,33 +21,35 @@
                 </li>
             </ul>
     </nav>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Item</th>
-                <th>Price</th>
-            </tr>
-            <tbody>
-                <?php
-                    $file = fopen("01.csv","r");
-                    while(($data = fgetcsv($file, 0, ","))!==FALSE){
-                        echo "<tr>";
-                        echo "<td>".$data[0]."</td>";
-                        echo "<td>".$data[1]."</td>";
-                    }
-                    fclose($file);
-                ?>
-            </tbody>
-        </thead>
-    </table>
-    <?php
-        $file = fopen("01.csv","r");
-        $balance =0;
-        while (($data = fgetcsv($file, 0, ",")) !== FALSE){
-            $balance += intval($data[1]);
-        }
-        fclose($file);
-        echo "Total is :". $balance;
-    ?>
+    <div clss="body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Item</th>
+                    <th>Price</th>
+                </tr>
+                <tbody>
+                    <?php
+                        $file = fopen("01.csv","r");
+                        while(($data = fgetcsv($file, 0, ","))!==FALSE){
+                            echo "<tr>";
+                            echo "<td>".$data[0]."</td>";
+                            echo "<td>".$data[1]."</td>";
+                        }
+                        fclose($file);
+                    ?>
+                </tbody>
+            </thead>
+        </table>
+        <?php
+            $file = fopen("01.csv","r");
+            $balance =0;
+            while (($data = fgetcsv($file, 0, ",")) !== FALSE){
+                $balance += intval($data[1]);
+            }
+            fclose($file);
+            echo "Total is :". $balance;
+        ?>
+    </div>
 </body>
 </html>
