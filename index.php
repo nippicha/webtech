eb1c5198c9fcc72085199399c2cfd53b6af0d624
nippicha <nippicha.c@ku.th>
2019-02-23 22:11:21 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <nav class="navbar navbar-expand-sm bg-light navbar-light">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="">Menu Bar</a>
+                <a class="nav-link" href="index.php">Menu Bar</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="01.csv" download="">Download</a>
