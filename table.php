2ce9c1dec991e67dcee2aa79256c438da0661995
nippicha <nippicha.c@ku.th>
2019-02-23 23:46:38 +0700

    Add files via upload

--- table.php
+++ table.php
@@ -7,6 +7,19 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Menu Bar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="01.csv" download="">Download</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="table.php">Show</a>
+                </li>
+            </ul>
+    </nav>
     <table>
         <thead>
             <tr>
